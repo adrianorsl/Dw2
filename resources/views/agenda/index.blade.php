8d11b67a1da4597cd8acef1e343d7af6cc3a106e
Adriano Zimmermann <adrianorslsc@hotmail.com>
2022-09-06 02:00:16 -0300

    Aulas do dia 05-09-22

--- resources/views/agenda/index.blade.php
+++ resources/views/agenda/index.blade.php
@@ -1,25 +1,28 @@
-<!DOCTYPE html>
-<?php
-    //session_start();
-    //$_SESSION['usuario'] = "Adriano";
-    
+@extends('agenda.layout')
 
-?>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
-<body>
-    
-    <?php
-    foreach($_SESSION['usuario'] as $dados){
-            var_dump($dados);
-            echo "<br>";
-               
-        }
-    ?>
-</body>
-</html>
+@section('titulo','Listar Usuários')
+
+@section('conteudo')
+<table>
+<tr><td><b>ID</b></td><td><b>Nome</b></td><td><b>Telefone</b></td><td><b>E-mail</b></td><td><b>Detalhes</b></td><td><b>Alterar</b></td><td><b>Excluir</b></td></tr>
+<?php
+    //var_dump($_SESSION)
+    if (isset($_SESSION['usuario'])){
+      foreach ($_SESSION['usuario'] as $dados) {
+        //var_dump($_SESSION['usuario']);
+        echo "<tr><td>{$dados['id']}</td>".
+             "<td>{$dados['nome']}</td>".
+             "<td>{$dados['telefone']}</td>".
+             "<td>{$dados['email']}</td>";?>
+             <td><a href="{{ route('agenda.show',$dados['id']) }}"><button>Detalhes</button></a></td>
+             <td><a href="{{ route('agenda.edit',$dados['id']) }}"><button>Editar</button></a></td>
+             <td><form id="form_delete" name="form_delete" action="{{ route('agenda.destroy',$dados['id']) }}" method="post" onsubmit="return confirm('Tem certeza que deseja excluir este registro?')">
+                @method('DELETE')
+                @csrf
+                <button type="submit">Excluir</button>
+            </form></td></tr><?php
+      }
+    }
+?>    
+</table>
+@endsection
--- resources/views/agenda/layout.blade.php
+++ resources/views/agenda/layout.blade.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('titulo')</title>
 </head>
 <body>
-    @component('agenda.nav')@endcomponent
 
-    @yield('conteudo')
-    
+@component('agenda.nav')@endcomponent
+
+@yield('conteudo')   
+
 </body>
 </html>
